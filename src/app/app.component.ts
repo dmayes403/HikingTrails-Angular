e26f919be210c4894160443d059383f66b85ec58
David Mayes <davidrmayes@gmail.com>
2018-12-04 19:30:51 -0700

    Added svg sanitizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hikingTrails';
+    constructor(
+        private matIconRegistry: MatIconRegistry,
+        private domSanitizer: DomSanitizer
+    ) {
+        this.matIconRegistry.addSvgIcon(
+            'outline_circle_check',
+            this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/outline-check-circle.svg')
+        );
+        this.matIconRegistry.addSvgIcon(
+            'outline_circle',
+            this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/outline-circle.svg')
+        );
+    }
+
+    title = 'hikingTrails';
 }
