9ccddcaacc12f0fcd57eee8f4a2dd9c9ce6ce73a
David Mayes <davidrmayes@gmail.com>
2018-12-06 21:54:23 -0700

    Getting trail by id

--- src/app/components/trail-details/trail-details.component.ts
+++ src/app/components/trail-details/trail-details.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { TrailsService } from '../../services/trails.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+
+import { TrailsService } from '../../services/trails.service';
+import { Trail } from '../../interfaces/trail';
+import { switchMap, tap, takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-trail-details',
     templateUrl: './trail-details.component.html',
     styleUrls: ['./trail-details.component.scss']
 })
-export class TrailDetailsComponent implements OnInit {
+export class TrailDetailsComponent implements OnInit, OnDestroy {
+    unsubscribe = new Subject();
+
+    trail: Trail;
 
     constructor(
         private trailsService: TrailsService,
@@ -15,7 +22,20 @@ export class TrailDetailsComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.trailsService.getTrailById(this.route.snapshot.params['id']).subscribe(data => console.log(data));
+        this.route.params.pipe(
+            takeUntil(this.unsubscribe),
+            switchMap(params => {
+                return this.trailsService.getTrailById(params['id']);
+            }),
+            tap(trail => {
+                console.log(trail);
+                this.trail = trail[0];
+            })
+        ).subscribe();
     }
 
+    ngOnDestroy() {
+        this.unsubscribe.next();
+        this.unsubscribe.complete();
+    }
 }
